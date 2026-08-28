83919e57586ada3298216a2b87eff250b6cd4146
joshuabowers <joshua.bowers+code@gmail.com>
2026-08-28 16:42:36 -0700

    [#2] Fixes issues preventing build mthmtcl is exluded from minimum-dependency-age checks for install; client should be wired to consume JSR published mthmtcl.

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,4 +5,9 @@ import solid from "vite-plugin-solid";
 export default defineConfig({
   plugins: [deno(), solid()],
   base: "/grphcl/",
+  resolve: {
+    alias: {
+      "jsr:@bowers/mthmtcl": "https://esm.jsr.io/@bowers/mthmtcl@0.0.1",
+    },
+  },
 });
