735e5ddbe79c6025bfd717a226a359f5d9258f58
joshuabowers <joshua.bowers+code@gmail.com>
2026-09-12 15:57:38 -0700

    feat: [#9] implements converse

--- packages/mthmtcl/functions/logical/converseImplication.test.ts
+++ packages/mthmtcl/functions/logical/converseImplication.test.ts
@@ -2,12 +2,75 @@ import { describe, it } from "node:test";
 import { expect } from "@std/expect";
 import { ConverseImplication, Variable } from "../../tree/mod.ts";
 import { boolean } from "../boolean.ts";
-import { real } from "../real.ts";
-import { complex } from "../complex.ts";
 import { variable } from "../variable.ts";
+import { not } from "../not.ts";
 import { converse } from "./converseImplication.ts";
 
 describe("converse", () => {
+  it("returns true when given two true things", () => {
+    expect(
+      converse(boolean(true), boolean(true)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns false if the left argument is false", () => {
+    expect(
+      converse(boolean(false), boolean(true)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns true if the right argument is false", () => {
+    expect(
+      converse(boolean(true), boolean(false)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns true if both arguments are false", () => {
+    expect(
+      converse(boolean(false), boolean(false)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns true if the left operand is true", () => {
+    expect(
+      converse(boolean(true), variable("x")),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns the left operand if the right is true", () => {
+    expect(
+      converse(variable("x"), boolean(true)),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the complement of the right if the left is false", () => {
+    expect(
+      converse(boolean(false), variable("x")),
+    ).toEqual(
+      not(variable("x")),
+    );
+  });
+
+  it("returns true if the right operand is false", () => {
+    expect(
+      converse(variable("x"), boolean(false)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
   it("is a ConverseImplication for unbound input", () => {
     expect(
       converse(variable("x"), variable("y")),
--- packages/mthmtcl/functions/logical/converseImplication.ts
+++ packages/mthmtcl/functions/logical/converseImplication.ts
@@ -1,9 +1,34 @@
-import { Boolean, ConverseImplication } from "../../tree/mod.ts";
+import { Boolean, ConverseImplication, Numeric } from "../../tree/mod.ts";
 import { Action, is } from "../../factories/factory.ts";
 import { binary, type BinaryFn, when } from "../../factories/binary.ts";
+import { boolean } from "../boolean.ts";
+import { _ } from "@arrows/multimethod";
+import { isValue } from "../../utility/deepEquals.ts";
 import { not } from "../not.ts";
+import { or } from "./disjunction.ts";
 
 export const converse: BinaryFn<
   ConverseImplication,
   Boolean
-> = binary(ConverseImplication, Boolean)();
+> = binary(ConverseImplication, Boolean)(
+  when([is(Numeric), is(Numeric)], (l, r) => [
+    or(l, not(r)),
+    Action.Application,
+  ]),
+  when(
+    [isValue(boolean(true)), _],
+    [boolean(true), Action.Annihilator],
+  ),
+  when(
+    [_, isValue(boolean(true))],
+    (l, _r) => [l, Action.Identity],
+  ),
+  when(
+    [isValue(boolean(false)), _],
+    (_l, r) => [not(r), Action.Conversion],
+  ),
+  when(
+    [_, isValue(boolean(false))],
+    [boolean(true), Action.Annihilator],
+  ),
+);
