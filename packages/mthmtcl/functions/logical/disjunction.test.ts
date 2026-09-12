ba7a8e361349cbfdf55c1342fedf8b44db4c3caf
joshuabowers <joshua.bowers+code@gmail.com>
2026-09-12 01:40:50 -0700

    feat: [#9] implements or

--- packages/mthmtcl/functions/logical/disjunction.test.ts
+++ packages/mthmtcl/functions/logical/disjunction.test.ts
@@ -5,9 +5,165 @@ import { boolean } from "../boolean.ts";
 import { real } from "../real.ts";
 import { complex } from "../complex.ts";
 import { variable } from "../variable.ts";
+import { not } from "../not.ts";
+import { and } from "./conjunction.ts";
+import { implies } from "./implication.ts";
+import { converse } from "./converseImplication.ts";
 import { or } from "./disjunction.ts";
 
 describe("or", () => {
+  it("returns true when given two true things", () => {
+    expect(
+      or(boolean(true), boolean(true)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns true if the left argument is true", () => {
+    expect(
+      or(boolean(true), boolean(false)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns true if the right argument is true", () => {
+    expect(
+      or(boolean(false), boolean(true)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns false if both arguments are false", () => {
+    expect(
+      or(boolean(false), boolean(false)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("casts reals to booleans, where 0 is false, non-zero is true", () => {
+    expect(
+      or(real(5), real(0)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("casts complexes to booleans, 0 => false, non-zero => true", () => {
+    expect(
+      or(complex(5, 0), complex(0, 0)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns the left operand if the right is false", () => {
+    expect(
+      or(variable("x"), boolean(false)),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the right operand if the left is false", () => {
+    expect(
+      or(boolean(false), variable("x")),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns true if the right operand is true", () => {
+    expect(
+      or(variable("x"), boolean(true)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns true if the left operand is true", () => {
+    expect(
+      or(boolean(true), variable("x")),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns the left operand if left is equivalent to right", () => {
+    expect(
+      or(variable("x"), variable("x")),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the left operand if the right is a left-child Conjunction of the left", () => {
+    expect(
+      or(variable("x"), and(variable("x"), variable("y"))),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the left operand if the right is a right-child Conjunction of the left", () => {
+    expect(
+      or(variable("x"), and(variable("y"), variable("x"))),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the right operand if the left is a left-child Conjunction of the right", () => {
+    expect(
+      or(and(variable("x"), variable("y")), variable("x")),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the right operand if the left is a right-child Conjunction of the right", () => {
+    expect(
+      or(and(variable("y"), variable("x")), variable("x")),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns true if the right operand is the negation of the left", () => {
+    expect(
+      or(variable("x"), not(variable("x"))),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns true if the left operand is the negation of the right", () => {
+    expect(
+      or(not(variable("x")), variable("x")),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns an implication if the left operand is a complement", () => {
+    expect(
+      or(not(variable("x")), variable("y")),
+    ).toEqual(
+      implies(variable("x"), variable("y")),
+    );
+  });
+
+  it("returns a converse if the right operand is a complement", () => {
+    expect(
+      or(variable("x"), not(variable("y"))),
+    ).toEqual(
+      converse(variable("x"), variable("y")),
+    );
+  });
+
   it("is a Disjunction for unbound input", () => {
     expect(
       or(variable("x"), variable("y")),
--- packages/mthmtcl/functions/logical/disjunction.ts
+++ packages/mthmtcl/functions/logical/disjunction.ts
@@ -1,9 +1,95 @@
-import { Boolean, Disjunction } from "../../tree/mod.ts";
+import {
+  Boolean,
+  Complement,
+  Complex,
+  Conjunction,
+  Disjunction,
+  Real,
+  TreeNode,
+} from "../../tree/mod.ts";
 import { Action, is } from "../../factories/factory.ts";
 import { binary, type BinaryFn, when } from "../../factories/binary.ts";
-import { not } from "../not.ts";
+import { boolean } from "../boolean.ts";
+import { converse } from "./converseImplication.ts";
+import { implies } from "./implication.ts";
+import { _ } from "@arrows/multimethod";
+import { deepEquals, isValue } from "../../utility/deepEquals.ts";
 
 export const or: BinaryFn<
   Disjunction,
   Boolean
-> = binary(Disjunction, Boolean)();
+> = binary(Disjunction, Boolean)(
+  when(
+    [is(Boolean), is(Boolean)],
+    (l, r) => [boolean(l.raw || r.raw), Action.Application],
+  ),
+  when([is(Complex), is(Complex)], (l, r) => [
+    boolean(
+      l.raw.a !== 0 || l.raw.b !== 0 ||
+        r.raw.a !== 0 || r.raw.b !== 0,
+    ),
+    Action.Application,
+  ]),
+  when(
+    [is(Real), is(Real)],
+    (l, r) => [boolean(l.raw !== 0 || r.raw !== 0), Action.Application],
+  ),
+  when(
+    [_, isValue(boolean(false))],
+    (l, _r) => [l, Action.Identity],
+  ),
+  when(
+    [isValue(boolean(false)), _],
+    (_l, r) => [r, Action.Identity],
+  ),
+  when(
+    [_, isValue(boolean(true))],
+    [boolean(true), Action.Annihilator],
+  ),
+  when(
+    [isValue(boolean(true)), _],
+    [boolean(true), Action.Annihilator],
+  ),
+  when(
+    deepEquals,
+    (l, _r) => [l, Action.Idemptency],
+  ),
+  when(
+    (l, r) => is(Conjunction)(r) && deepEquals(l, r.left),
+    (l, _r) => [l, Action.Absorption],
+  ),
+  when(
+    (l, r) => is(Conjunction)(r) && deepEquals(l, r.right),
+    (l, _r) => [l, Action.Absorption],
+  ),
+  when(
+    (l, r) => is(Conjunction)(l) && deepEquals(l.left, r),
+    (_l, r) => [r, Action.Absorption],
+  ),
+  when(
+    (l, r) => is(Conjunction)(l) && deepEquals(l.right, r),
+    (_l, r) => [r, Action.Absorption],
+  ),
+  when(
+    (l, r) => is(Complement)(r) && deepEquals(l, r.child),
+    [boolean(true), Action.Tautology],
+  ),
+  when(
+    (l, r) => is(Complement)(l) && deepEquals(l.child, r),
+    [boolean(true), Action.Tautology],
+  ),
+  when(
+    [is(Complement), is(TreeNode)],
+    (l, r) => [
+      implies(l.child, r),
+      Action.Conversion,
+    ],
+  ),
+  when(
+    [is(TreeNode), is(Complement)],
+    (l, r) => [
+      converse(l, r.child),
+      Action.Conversion,
+    ],
+  ),
+);
