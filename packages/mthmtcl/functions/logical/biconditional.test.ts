558bfa725649cf7c7946f52889ae1e8fe3baead0
joshuabowers <joshua.bowers+code@gmail.com>
2026-09-12 15:51:48 -0700

    feat: [#9] implements xnor

--- packages/mthmtcl/functions/logical/biconditional.test.ts
+++ packages/mthmtcl/functions/logical/biconditional.test.ts
@@ -2,12 +2,83 @@ import { describe, it } from "node:test";
 import { expect } from "@std/expect";
 import { Biconditional, Variable } from "../../tree/mod.ts";
 import { boolean } from "../boolean.ts";
-import { real } from "../real.ts";
-import { complex } from "../complex.ts";
 import { variable } from "../variable.ts";
+import { not } from "../not.ts";
 import { xnor } from "./biconditional.ts";
 
 describe("xnor", () => {
+  it("returns true when given two true things", () => {
+    expect(
+      xnor(boolean(true), boolean(true)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns false if the left argument is false", () => {
+    expect(
+      xnor(boolean(false), boolean(true)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns false if the right argument is false", () => {
+    expect(
+      xnor(boolean(true), boolean(false)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns true if both arguments are false", () => {
+    expect(
+      xnor(boolean(false), boolean(false)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns the right operand if the left is true", () => {
+    expect(
+      xnor(boolean(true), variable("x")),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the left operand if the right is true", () => {
+    expect(
+      xnor(variable("x"), boolean(true)),
+    ).toEqual(
+      variable("x"),
+    );
+  });
+
+  it("returns the complement of the right if the left is false", () => {
+    expect(
+      xnor(boolean(false), variable("x")),
+    ).toEqual(
+      not(variable("x")),
+    );
+  });
+
+  it("returns the complement of the left if the right is false", () => {
+    expect(
+      xnor(variable("x"), boolean(false)),
+    ).toEqual(
+      not(variable("x")),
+    );
+  });
+
+  it("returns true if the operands are equal", () => {
+    expect(
+      xnor(variable("x"), variable("x")),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
   it("is a Biconditional for unbound input", () => {
     expect(
       xnor(variable("x"), variable("y")),
--- packages/mthmtcl/functions/logical/biconditional.ts
+++ packages/mthmtcl/functions/logical/biconditional.ts
@@ -1,9 +1,36 @@
-import { Biconditional, Boolean } from "../../tree/mod.ts";
+import { Biconditional, Boolean, Numeric } from "../../tree/mod.ts";
 import { Action, is } from "../../factories/factory.ts";
 import { binary, type BinaryFn, when } from "../../factories/binary.ts";
+import { boolean } from "../boolean.ts";
+import { _ } from "@arrows/multimethod";
+import { deepEquals, isValue } from "../../utility/deepEquals.ts";
 import { not } from "../not.ts";
+import { and } from "./conjunction.ts";
+import { implies } from "./implication.ts";
 
 export const xnor: BinaryFn<
   Biconditional,
   Boolean
-> = binary(Biconditional, Boolean)();
+> = binary(Biconditional, Boolean)(
+  when([is(Numeric), is(Numeric)], (l, r) => [
+    and(implies(l, r), implies(r, l)),
+    Action.Application,
+  ]),
+  when(
+    [isValue(boolean(true)), _],
+    (_l, r) => [r, Action.Identity],
+  ),
+  when(
+    [_, isValue(boolean(true))],
+    (l, _r) => [l, Action.Identity],
+  ),
+  when(
+    [isValue(boolean(false)), _],
+    (_l, r) => [not(r), Action.Conversion],
+  ),
+  when(
+    [_, isValue(boolean(false))],
+    (l, _r) => [not(l), Action.Conversion],
+  ),
+  when(deepEquals, [boolean(true), Action.Annihilator]),
+);
