b3004f55c63d09c42e257e1b0afd1e7985b0cfdb
joshuabowers <joshua.bowers+code@gmail.com>
2026-09-12 15:42:01 -0700

    feat: [#9] implements nor

--- packages/mthmtcl/functions/logical/jointDenial.test.ts
+++ packages/mthmtcl/functions/logical/jointDenial.test.ts
@@ -2,12 +2,108 @@ import { describe, it } from "node:test";
 import { expect } from "@std/expect";
 import { JointDenial, Variable } from "../../tree/mod.ts";
 import { boolean } from "../boolean.ts";
-import { real } from "../real.ts";
-import { complex } from "../complex.ts";
 import { variable } from "../variable.ts";
+import { not } from "../not.ts";
+import { and } from "./conjunction.ts";
 import { nor } from "./jointDenial.ts";
 
 describe("nor", () => {
+  it("returns false when given two true things", () => {
+    expect(
+      nor(boolean(true), boolean(true)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns false if the left argument is false", () => {
+    expect(
+      nor(boolean(false), boolean(true)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns false if the right argument is false", () => {
+    expect(
+      nor(boolean(true), boolean(false)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns true if both arguments are false", () => {
+    expect(
+      nor(boolean(false), boolean(false)),
+    ).toEqual(
+      boolean(true),
+    );
+  });
+
+  it("returns the complement of the left operand if the right is false", () => {
+    expect(
+      nor(variable("x"), boolean(false)),
+    ).toEqual(
+      not(variable("x")),
+    );
+  });
+
+  it("returns the complement of the right operand if the left is false", () => {
+    expect(
+      nor(boolean(false), variable("x")),
+    ).toEqual(
+      not(variable("x")),
+    );
+  });
+
+  it("returns false if the right operand is true", () => {
+    expect(
+      nor(variable("x"), boolean(true)),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns false if the left operand is true", () => {
+    expect(
+      nor(boolean(true), variable("x")),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns the complement of the left operand if left is equivalent to right", () => {
+    expect(
+      nor(variable("x"), variable("x")),
+    ).toEqual(
+      not(variable("x")),
+    );
+  });
+
+  it("returns a conjunction of mutually complemented operands", () => {
+    expect(
+      nor(not(variable("x")), not(variable("y"))),
+    ).toEqual(
+      and(variable("x"), variable("y")),
+    );
+  });
+
+  it("returns false if the right operand is the complement of the left", () => {
+    expect(
+      nor(variable("x"), not(variable("x"))),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
+  it("returns false if the left operand is the complement of the right", () => {
+    expect(
+      nor(not(variable("x")), variable("x")),
+    ).toEqual(
+      boolean(false),
+    );
+  });
+
   it("is a JointDenial for unbound input", () => {
     expect(
       nor(variable("x"), variable("y")),
--- packages/mthmtcl/functions/logical/jointDenial.ts
+++ packages/mthmtcl/functions/logical/jointDenial.ts
@@ -1,9 +1,54 @@
-import { Boolean, JointDenial } from "../../tree/mod.ts";
+import { Boolean, Complement, JointDenial, Numeric } from "../../tree/mod.ts";
 import { Action, is } from "../../factories/factory.ts";
 import { binary, type BinaryFn, when } from "../../factories/binary.ts";
+import { boolean } from "../boolean.ts";
+import { _ } from "@arrows/multimethod";
+import { deepEquals, isValue } from "../../utility/deepEquals.ts";
 import { not } from "../not.ts";
+import { and } from "./conjunction.ts";
+import { or } from "./disjunction.ts";
 
 export const nor: BinaryFn<
   JointDenial,
   Boolean
-> = binary(JointDenial, Boolean)();
+> = binary(JointDenial, Boolean)(
+  when([is(Numeric), is(Numeric)], (l, r) => [
+    not(or(l, r)),
+    Action.Application,
+  ]),
+  when(
+    [_, isValue(boolean(true))],
+    [boolean(false), Action.Annihilator],
+  ),
+  when(
+    [isValue(boolean(true)), _],
+    [boolean(false), Action.Annihilator],
+  ),
+  when(
+    [_, isValue(boolean(false))],
+    (l, _r) => [not(l), Action.Conversion],
+  ),
+  when(
+    [isValue(boolean(false)), _],
+    (_l, r) => [not(r), Action.Conversion],
+  ),
+  when(
+    deepEquals,
+    (l, _r) => [not(l), Action.Conversion],
+  ),
+  when(
+    [is(Complement), is(Complement)],
+    (l, r) => [
+      and(l.child, r.child),
+      Action.DeMorgan,
+    ],
+  ),
+  when(
+    (l, r) => is(Complement)(r) && deepEquals(l, r.child),
+    [boolean(false), Action.Contradiction],
+  ),
+  when(
+    (l, r) => is(Complement)(l) && deepEquals(l.child, r),
+    [boolean(false), Action.Contradiction],
+  ),
+);
